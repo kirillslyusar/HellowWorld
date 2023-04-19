cf2e80151295125b17dd9dd207b74f772b897b52
kirillslyusar <kirillslyusar@gmail.com>
2023-04-19 19:24:15 +0300

    Initial Commit

---
+++ HellowWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HellowWorld
+//
+//  Created by Кирилл Слюсарь on 19.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
